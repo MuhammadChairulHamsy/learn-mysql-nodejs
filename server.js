a52546a47283b4b464e69a4c1278ef134b8f9c9e
HamsyDev <mach.hamsy@gmail.com>
2026-01-09 08:01:58 +0700

    fix views path

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import path from 'path'
+import path from "path";
 import { fileURLToPath } from "url";
 import mahasiswaRoute from "./src/routes/mahasiswa.route.js";
 import pageMahasiswaRoute from "./src/routes/pageMahasiswa.route.js";
@@ -9,24 +9,25 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// middleware
 app.use(express.urlencoded({ extended: true }));
+
+// VIEW ENGINE
 app.set("view engine", "ejs");
-app.set("views", "views"); 
+app.set("views", path.join(__dirname, "views")); // 🔥 FIX UTAMA
 
-app.use(express.static(path.join(__dirname, "public")))
+// static file
+app.use(express.static(path.join(__dirname, "public")));
 
-// API → JSON
+// routes
 app.use("/api/mahasiswa", mahasiswaRoute);
-
-// PAGE → EJS
 app.use("/", pageMahasiswaRoute);
 
+// local only
 if (process.env.NODE_ENV !== "production") {
   app.listen(8000, () => {
     console.log("Server running at http://localhost:8000");
   });
 }
+
 export default app;
-
-
-
